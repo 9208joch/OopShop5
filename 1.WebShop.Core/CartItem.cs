3480b80e32e3cebf460dbeac54147dfe19ec8e80
niklaskaapa <niklas.kaapa@gmail.com>
2026-03-31 22:50:29 +0200

    Shopping Cart

---
+++ 1.WebShop.Core/CartItem.cs
@@ -0,0 +1,28 @@
+﻿using _1.WebShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1.WebShop.Core
+{
+    public class CartItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+
+        public CartItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+        public decimal GetTotalPrice()
+        {
+            return Product.Price * Quantity;
+        }
+
+
+
+    }
+}
