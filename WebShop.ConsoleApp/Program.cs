ba882612086b265fcad541c6b146f20c336611df
9208joch <9208joch@gapp.uddevalla.se>
2026-04-01 09:42:33 +0200

    tog bort test

--- WebShop.ConsoleApp/Program.cs
+++ WebShop.ConsoleApp/Program.cs
@@ -15,30 +15,3 @@ services.AddDbContext<WebShopContext>(options =>
 services.AddScoped<IProductRepository, ProductRepository>();
 
 var provider = services.BuildServiceProvider();
-// testkod
-using (var scope = provider.CreateScope())
-{
-    var repo = scope.ServiceProvider.GetRequiredService<IProductRepository>();
-
-    if (repo is ProductRepository concreteRepo)
-    {
-       
-        await concreteRepo.SeedAsync();
-
-
-        var products = await concreteRepo.GetAllAsync();
-
-        Console.WriteLine("=== PRODUCTS ===\n");
-
-        foreach (var p in products)
-        {
-            Console.WriteLine($"{p.Id} | {p.Name} ({p.Size})");
-            Console.WriteLine($"Price: {p.Price} kr");
-            Console.WriteLine($"Stock: {p.Inventory}");
-            Console.WriteLine("----------------------");
-        }
-    }
-}
-
-Console.WriteLine("\nTryck valfri knapp...");
-Console.ReadKey();
