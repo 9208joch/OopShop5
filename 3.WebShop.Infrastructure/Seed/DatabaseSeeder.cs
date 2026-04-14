dc2ec1e2281cd27cd763ba8680eb70ab11bdcd97
9208joch <9208joch@gapp.uddevalla.se>
2026-04-14 11:28:27 +0200

    gort order seeder

---
+++ 3.WebShop.Infrastructure/Seed/DatabaseSeeder.cs
@@ -0,0 +1,41 @@
+﻿using _1.WebShop.Core.Entities;
+using _3.WebShop.Infrastructure.Seed;
+
+namespace _3.WebShop.Infrastructure.DbContext
+{
+    public static class DatabaseSeeder
+    {
+        public static async Task SeedAsync(WebShopContext context)
+        {
+            //  1. PRODUCTS
+            if (!context.Products.Any())
+            {
+                var Products = ProductSeeder.GenerateProducts(200);
+                context.Products.AddRange(Products);
+                await context.SaveChangesAsync();
+            }
+
+            //  2. CUSTOMERS (måste finnas)
+            var customers = context.Customers.ToList();
+
+            if (!customers.Any())
+            {
+                throw new Exception("No customers found in database. Seed customers first.");
+            }
+
+            //  3. HÄMTA PRODUCTS FRÅN DB (med riktiga Id:n)
+            var products = context.Products.ToList();
+
+            //  4. ORDERS
+            if (!context.Orders.Any())
+            {
+                var orders = OrderSeeder.GenerateOrders(products, customers, 10);
+
+                Console.WriteLine($"[Seeder] Orders created: {orders.Count}");
+
+                context.Orders.AddRange(orders);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- WebShop.ConsoleApp/Program.cs
+++ WebShop.ConsoleApp/Program.cs
@@ -1,9 +1,8 @@
-﻿using _2.WebShop.Application;
-using _1.WebShop.Core.Interfaces;
+﻿using _1.WebShop.Core.Interfaces;
 using _2.WebShop.Application.Services;
 using _2.WebShop.Application.UseCases;
-using _3.WebShop.Infrastructure.DbContext;
 using _3.WebShop.Infrastructure.Api;
+using _3.WebShop.Infrastructure.DbContext;
 using _3.WebShop.Infrastructure.Payments;
 using _3.WebShop.Infrastructure.Repositories;
 using _3.WebShop.Infrastructure.Shipping;
@@ -13,57 +12,6 @@ using Microsoft.Extensions.DependencyInjection;
 using UI;
 using WebShop.ConsoleApp.UI;
 
-var services = new ServiceCollection();
-
-services.AddScoped<ShopMenu>();
-
-services.AddDbContext<WebShopContext>(options =>
-    options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=WebShopDb;Trusted_Connection=True;"));
-
-services.AddScoped<IProductRepository, ProductRepository>();
-services.AddSingleton<CartService>();
-services.AddScoped<ShoppingCartMenu>();
-services.AddScoped<CheckoutService>();
-
-services.AddScoped<CheckoutFlow>();
-services.AddScoped<ConsoleCheckoutFlow>();
-
-services.AddScoped<IPaymentMethod, CardPayment>();
-services.AddScoped<IPaymentMethod, SwishPayment>();
-
-services.AddScoped<IShippingOption, StandardShipping>();
-services.AddScoped<IShippingOption, ExpressShipping>();
-services.AddScoped<IShippingOption, NoShippingPickupStore>();
-services.AddScoped<AdminMenu>();
-services.AddScoped<ICustomerRepository, CustomerRepository>();
-
-
-services.AddScoped<IOrderRepository, OrderRepository>();        //NK
-//var provider = services.BuildServiceProvider();
-
-services.AddScoped<Menu>();
-services.AddSingleton<ConsoleNavigationService>();
-
-
-    services.AddHttpClient<IDistanceService, DistanceService>();
-
-var provider = services.BuildServiceProvider();
-
-using (var scope = provider.CreateScope())
-{
-    var repo = scope.ServiceProvider.GetRequiredService<IProductRepository>();
-
-    if (repo is ProductRepository concreteRepo)
-    {
-        await concreteRepo.SeedAsync();
-    }
-
-   var menu = scope.ServiceProvider.GetRequiredService<Menu>();
-   await menu.Start();
-
-}
-
-
 namespace WebShop.ConsoleApp
 {
     internal class Program
@@ -72,21 +20,56 @@ namespace WebShop.ConsoleApp
         {
             var services = new ServiceCollection();
 
-            services.AddDbContext<WebShopContext>();
-            services.AddScoped<IProductRepository, ProductRepository>();
+            //  DbContext
+            services.AddDbContext<WebShopContext>(options =>
+                options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=WebShopDb;Trusted_Connection=True;"));
 
-            services.AddScoped<_1.WebShop.Core.Interfaces.ICustomerRepository,
-                _3.WebShop.Infrastructure.Repositories.CustomerRepository>();
-            
+            //  Repositories
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            //  Services
+            services.AddSingleton<CartService>();
+            services.AddScoped<CheckoutService>();
+
+            //  Flows
+            services.AddScoped<CheckoutFlow>();
+            services.AddScoped<ConsoleCheckoutFlow>();
+
+            //  Payment
+            services.AddScoped<IPaymentMethod, CardPayment>();
+            services.AddScoped<IPaymentMethod, SwishPayment>();
+
+            //  Shipping
+            services.AddScoped<IShippingOption, StandardShipping>();
+            services.AddScoped<IShippingOption, ExpressShipping>();
+            services.AddScoped<IShippingOption, NoShippingPickupStore>();
+
+            //  UI
+            services.AddScoped<Menu>();
+            services.AddScoped<ShopMenu>();
+            services.AddScoped<ShoppingCartMenu>();
             services.AddScoped<AdminMenu>();
             services.AddSingleton<ConsoleNavigationService>();
 
-            // 1. Bygg ihop alla services
-            var serviceProvider = services.BuildServiceProvider();
+            //  API
+            services.AddHttpClient<IDistanceService, DistanceService>();
 
-            // 3. Starta din Admin-meny!
-            var adminMenu = serviceProvider.GetRequiredService<AdminMenu>();
-            await adminMenu.ShowMenuAsync();
+            var provider = services.BuildServiceProvider();
+
+            using (var scope = provider.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<WebShopContext>();
+
+
+                //  Seed database
+                await DatabaseSeeder.SeedAsync(context);
+
+                //  Starta appen
+                var menu = scope.ServiceProvider.GetRequiredService<Menu>();
+                await menu.Start();
+            }
         }
     }
 }
